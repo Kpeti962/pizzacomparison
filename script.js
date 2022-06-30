87a409462d5ecc6fbe90009cc7b1ab94c96c865f
Peti <kpeti962@gmailcom>
2022-06-30 16:41:22 +0200

    some repair

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const price2 = document.getElementById("secondPrice");
 const size1 = document.getElementById("firstSize");
 const size2 = document.getElementById("secondSize");
 
+const number1 = document.getElementById("firstNumber");
+const number2 = document.getElementById("secondNumber");
+
 const result1 = document.getElementById("outputCompare1");
 const result2 = document.getElementById("outputCompare2");
 
@@ -11,20 +14,22 @@ const btn = document.querySelector(".btnInput");
 const btn2 = document.querySelector(".btnInput2");
 const winner = document.getElementById("outputWinner");
 
-btn.addEventListener("click", function () {
+
+
+ const start = function () {
   const size1Area = ((size1.value / 2) ** 2 * Math.PI).toFixed();
   const size2Area = ((size2.value / 2) ** 2 * Math.PI).toFixed();
 
-  const price1forOne = (price1.value / size1Area).toFixed(2);
-  const price2forOne = (price2.value / size2Area).toFixed(2);
-
-  result1.innerText = `${price1forOne} Ft/cm²`;
-  result2.innerText = `${price2forOne} Ft/cm²`;
+  const price1forOne = (price1.value / size1Area * number1.value).toFixed(2);
+  const price2forOne = (price2.value / size2Area * number2.value).toFixed(2);
+  result1.classList.remove("hidden") && result2.classList.remove("hidden")
+      result1.innerText = `${price1forOne} Ft/cm²`;
+      result2.innerText = `${price2forOne} Ft/cm²`;
 
   if (price1forOne < price2forOne) {
-    winner.innerText = `Az első pizza jobban megéri te sóher geci`;
+    winner.innerText = `Az első pizza éri meg jobban`;
   } else if (price1forOne > price2forOne) {
-    winner.innerText = `A második pizza éri meg jobban, de ha a drágábbat választod most az egyszer, attól még sóher maradsz`;
+    winner.innerText = `A második pizza éri meg jobban`;
   }
 
   if (
@@ -34,19 +39,26 @@ btn.addEventListener("click", function () {
     size2.value <= 0
   ) {
     alert(
-      "Nem elpofáztam, hogy írj be mindenhova pozitív számokat, hogyha eredményt akarsz??!!"
-    );
+      "Írj be mindenhova pozitív számokat az eredményhez"
+    ) 
   }else {
-
       btn2.classList.remove("hidden");
+  
   }
-});
+} 
+
+
+
+btn.addEventListener("click", start);
 
 btn2.addEventListener("click", function () {
     document.getElementById("firstPrice").value = ""
     document.getElementById("secondPrice").value = ""
     document.getElementById("firstSize").value = ""
     document.getElementById("secondSize").value = ""
+    document.getElementById("firstNumber").value = ""
+    document.getElementById("secondNumber").value = ""
+
   result1.innerText = "";
   result2.innerText = "";
   winner.innerText = ""
